c3c2198f4deb49398859f3f8ed32dbf9a1735da9
meisme-dev <omar.elantil@gmail.com>
2022-10-10 20:56:37 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2022 meisme <meisme.mail@pm.me>
+ * 
+ * This file is part of vkcr.
+ * 
+ * vkcr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * vkcr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with vkcr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use vkcr::core;
+
+fn main() {
+    core::game::Game::init();
+}
